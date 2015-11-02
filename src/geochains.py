4b991e34f437f3b3211358b30874102bf6997e1d
Oral Okan <oralokan@gmail.com>
2015-11-02 19:34:16 +0200

    Now to actually do the queries

--- src/geochains.py
+++ src/geochains.py
@@ -3,6 +3,7 @@
 # date: november 2, 2015
 # modified: november 2, 2015
 
+import re
 from expander import Expander
 from substitution_rule import SubstitutionRule 
 
@@ -10,13 +11,28 @@ PATTERN=r'\[.*?\]'      # using non-greedy qualifier
 
 class GeochainsSubstitutionRule(SubstitutionRule):
     def values_for_match(self, matched):
-        print matched
-        if matched == r'[D]':
-            return ['1','2']
+        try:
+            query_type = re.compile(r'\[.*?:').search(matched).group()[1:-1].strip()
+            query_list = re.compile(r':.*?\]').search(matched).group()[1:-1].strip().split(',')
+        except AttributeError:
+            print "ERROR: Malformed pattern: {}".format(matched)
+            raise SystemExit
+        
+        if query_type == 'CN':
+            return self.country_search(query_list)
 
-        if matched == r'[L]':
-            return ['a', 'b']
+        elif query_type == 'AS':
+            return self.country_search(query_list)
 
+        else:
+            print "ERROR: Malformed pattern: {}".format(matched)
+            raise SystemExit
+
+    def country_search(self, patterns):
+        return ["IPs of countries:{}".format(','.join(patterns))]
+
+    def as_search(self, patterns):
+        return ["IPs of ASs:{}".format(','.join(patterns))]
 
 
 class Geochains:
@@ -26,14 +42,11 @@ class Geochains:
         self.sub_rule = GeochainsSubstitutionRule()
         expander = Expander(self.pattern, self.sub_rule)
 
-        print expander
-        print expander.pattern
-        print expander.substitution_rule
-
         sample_input = \
-r'''[D] is a digit, '[L]' is a letter
-Again, '[L]' is a letter
-And this line stays the same'''
+r'''
+[CN:turkey,usa] 
+[AS:13,421]
+'''
 
         output = expander.expand(sample_input)
 
